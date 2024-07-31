3cc3bf9f72eb3575dbcb54bf7c6ca01f841e965a
akshayabankumidi <akshayabankumidi@gmail.com>
2024-07-31 21:52:07 +0530

    revert

--- src/controllers/testController.js
+++
@@ -1,8 +0,0 @@
-exports.sayHello = async (req, res, next) => {
-    try {
-
-      res.status(201).json({message: 'hello '});
-    } catch (err) {
-      next(err);
-    }
-  };
--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -7,5 +7,4 @@ router.get('/', taskController.getAllTasks);
 router.get('/:id', taskController.getTaskById);
 router.put('/:id', taskController.updateTask);
 router.delete('/:id', taskController.deleteTask);
-router.get('/',testController.sayHello);
 module.exports = router;
